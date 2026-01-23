3dd67664079a23e410b5f95c8c8db34ff50ea7dc
magjwv1212-debug <magjwv1212@gmail.com>
2026-01-23 10:27:59 +0100

    kolon

---
+++ sjakk.js
@@ -0,0 +1,11 @@
+let bondeData = {
+    bonde: { rad: 6, kol: 1 },
+    bonde1: { rad: 6, kol: 3 },
+    bonde2: { rad: 6, kol: 5 },
+    bonde3: { rad: 6, kol: 7 },
+    bonde4: { rad: 0, kol: 0 },
+    bonde5: { rad: 0, kol: 2 },
+    bonde6: { rad: 0, kol: 4 },
+    bonde7: { rad: 0, kol: 6 }
+
+};
